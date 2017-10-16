41c384dadb271d5e8d0655025394c2ac5d6707ea
Byrone Collins <byronecollins@Byrones-MacBook-Pro.local>
2017-10-16 12:14:54 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import sun.misc.Request;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String LoadFormPage(){
+        return "formtemplate";
+    }
+
+
+    @RequestMapping("/processform")
+    public String loadFromPage(@RequestParam("login") String Login,
+                               Model model){
+        model.addAttribute("loginval", Login);
+        return "confirm";
+    }
+}
